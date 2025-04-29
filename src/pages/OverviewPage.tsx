021ce2cfff68fff0698e92b853dcf2db66b61602
CauanLagrotta <caun06lagrotta@gmail.com>
2025-04-29 16:27:57 -0300

    feat: fazendo o grafico de pizza

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion";
 import { StatCard } from "../components/common/StatCard";
 import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
 import { SalesOverviewChart } from "../components/overview/SalesOverviewChart";
+import { CategoryDistributionChart } from "../components/overview/CategoryDistributionChart";
+import { SalesChannelChart } from "../components/overview/SalesChannelChart";
 
 export function OverviewPage() {
   return (
@@ -46,6 +48,8 @@ export function OverviewPage() {
         {/* CHARTS */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
             <SalesOverviewChart />
+            <CategoryDistributionChart />
+            <SalesChannelChart />
         </div>
 
       </main>
